605b103186d443077f4c5838f19630412b23a7cd
pmgrollemund <paul.marie.grollemund@gmail.com>
2024-06-05 11:28:27 +0200

    change in printing class SICPLN

--- demo/demo_spider.R
+++ demo/demo_spider.R
@@ -18,7 +18,10 @@ if (length(args) < 2) {
 
 # général options
 output <- args[1]
-code_path <- args[2]
+code_path <- args
+
+output <- "result"
+code_path <- "../code/SICPLN_Pack.R"
 
 
 # Créer le répertoire de sortie
@@ -136,100 +139,5 @@ ggsave(file.path(output_dir, "density_abundance_sicpln.pdf"), density_abundance_
 ggsave(file.path(output_dir, "precision_mat_sicpln.pdf"), precision_mat_sicpln, width = 10, height = 6, units = "in")
 ggsave(file.path(output_dir, "MSE_pln_graph.pdf"), MSE_pln_graph, width = 10, height = 6, units = "in")
 ggsave(file.path(output_dir, "MSE_sicpln_graph.pdf"), MSE_sicpln_graph, width = 10, height = 6, units = "in")
-
-save.image(file.path(output, "Renv.RData"))
-
-e des VIF
-#retain_Covar <- Covariate[, Res_vif_A_Ref$selected_variables]
-retain_Covarr <- prep_spider[, Res_vif_A_Ref$selected_variables]
-
-rownames(retain_Covarr) <- rownames(prep_spider$Abundance)
-
-
-data_tmp <- list(Abundance = prep_spider$Abundance, Covariate = retain_Covarr)
-data_to_PLN <- prepare_data(data_tmp$Abundance, data_tmp$Covariate)
-data_to_PLN <- data_to_PLN[, -ncol(data_to_PLN)]
-
-
-
-# Spécifiez les paramètres de contrôle
-control <- PLN_param(backend = "nlopt", covariance = "full")
-
-###### Cas avec PLN (spider) : ----
-# Ajustement du modèle PLN (Poisson-Lognormal) avec la surface en tant qu'offset
-cat("\n\n\t lancement du modèle PLN.\n")
-cat("        ==========================\n")
-
-spider_pln <- PLNmodels::PLN(Abundance ~ . , control = control, data = data_to_PLN) 
-spider_pln$model_par$B
-
-
-# Ajustement pour la selection de variable
-cat("\n\n\t lancement du modèle SICPLN.\n")
-cat("        ============================\n")
-
-formule <- Abundance ~ .
-data <- data_to_PLN
-
-spider_sicpln <- SICPLN(formule , data = data_to_PLN, control = control)
-print(spider_sicpln$res_fisher$model_par$B)
-# , offset_column_name = "FallTwig"
-
-# Convertir la matrice en un format adapté à ggplot2
-data_df <- melt(data$Abundance)
-data_sic <- melt(spider_sicpln$fitted)
-
-
-# Renommer les colonnes
-colnames(data_df) <- c("Site", "Species", "Comptage")
-colnames(data_sic) <- c("Site", "Species", "Comptage")
-
-# Graphiques
-plot_coef_pln <- coef_genus_barre(spider_pln$model_par$B[-1,], 
-                                  axis_names = c("Spider", "Variables", "Coefficients value", "PLN"), data)
-plot_coef_sicpln <- coef_genus_barre(spider_sicpln$res_fisher$model_par$B[-1,],
-                                     axis_names = c("Spider species", "Variables", "Coefficients value", "SICPLN"), data)
-
-
-
-plt_abund <- boxplot_graph(data_df = data_df, x = "Species", y = "Comptage", title = "Boxplot of Abundances by Species")
-plt_abund_sic <- boxplot_graph(data_df = data_sic, x = "Species", y = "Comptage", title = "Boxplot of Abundances fitted by Species")
-
-# Utilisation de la fonction avec les données d'abondance et les valeurs prédites
-resid_pln <- plot_residus_density(data$Abundance, spider_pln$fitted, output_dir) #, output_dir
-resid_sicpln <- plot_residus_density(data$Abundance, spider_sicpln$fitted, output_dir) #, output_dir
-
-# # density des y et densité des y prédit
-density_abundance_pln <- plot_density(data$Abundance, spider_pln$fitted, main = "Density Plot of Abundance from PLN", xlab = "Abundance")
-density_abundance_sicpln <- plot_density(data$Abundance, spider_sicpln$fitted, main = "Density Plot of Abundance from SICPLN", xlab = "Abundance")
-
-# image de la matrice sigma (precision matrix)
-tmp <- solve(spider_sicpln$res_fisher$model_par$Sigma)
-norm <- sqrt(diag(tmp)) %*% t(sqrt(diag(tmp)))
-cor_mat <- tmp / norm
-precision_mat_sicpln <- plot_precision_matrix(cor_mat, axis_names = c("", "", "", "SICPLN_Spider precision matrix of Sigma"), limit = NULL)
-
-# Appel de la fonction plot_mse
-MSE_pln_graph <- plot_mse_barplot(data$Abundance, spider_pln$fitted)
-MSE_sicpln_graph <- plot_mse_barplot(data$Abundance, spider_sicpln$fitted)
-
-# # Relation entre variable environnementale et Espèce
-plotcovar_Abund <- plot_abundance_vs_environment(spider_sicpln$res_fisher$model_par$B, data, output_dir) #, output_dir
-
-
-combined_density_plot <- plot_combined_density(data$Abundance, spider_pln$fitted,spider_sicpln$fitted,
-                                               main = "Combined Density Plot of Abundance", xlab = "Abundance")
-  
-# Sauvegarde des graphiques dans le dossier de sortie spécifié en argument
-ggsave(file.path(output_dir, "plot_pln.pdf"), plot_coef_pln, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "plot_sicpln.pdf"), plot_coef_sicpln, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "plt_abund.pdf"), plt_abund, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "plt_abund_sic.pdf"), plt_abund_sic, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "density_abundance_pln.pdf"), density_abundance_pln, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "density_abundance_sicpln.pdf"), density_abundance_sicpln, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "precision_mat_sicpln.pdf"), precision_mat_sicpln, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "MSE_pln_graph.pdf"), MSE_pln_graph, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "MSE_sicpln_graph.pdf"), MSE_sicpln_graph, width = 10, height = 6, units = "in")
-ggsave(file.path(output_dir, "combined_density_plot.pdf"), combined_density_plot, width = 10, height = 6, units = "in")
 
 save.image(file.path(output, "Renv.RData"))
