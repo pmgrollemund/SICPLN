e1b2745afa2e720eb455bc323e5ec1f6d4e5557a
REMESHA <118030182+REMESHA@users.noreply.github.com>
2024-07-23 10:41:31 +0200

    Update demo_genus.R

--- demo/demo_genus.R
+++ demo/demo_genus.R
@@ -30,8 +30,8 @@ if (!dir.exists(output_dir)) {
 
 # Importer les fonctions
 source(code_path)
-# source("/home/gdremesh/Bureau/new_code/mon_code_fonction.R")
-# load("/home/gdremesh/Bureau/new_code/genus2.RData")
+#source("/home/gdremesh/Bureau/new_code/demo_genus/SICPLN_Pack.R")
+#load("/home/gdremesh/Bureau/new_code/demo_genus/genus2.RData")
 # Chargement de la base de données à partir du premier argument
 load(data_path)
 data <- genus2
@@ -41,12 +41,12 @@ Abundance <- genus2[, 1:15]
 Covariate <- genus2[, -c(1:15)]
 
 # Génération des variables environnementales catégorielles
-var_cat <- as.factor(matrix(sample(letters[1:4], nrow(Covariate), replace = TRUE), ncol = 1))
-cat1 = as.factor(matrix(sample(letters[1:3], nrow(Covariate), replace = TRUE), ncol = 1))
-cat2 = as.factor(matrix(sample(LETTERS[1:2], nrow(Covariate), replace = TRUE), ncol = 1))
-cat3 = as.factor(ifelse(cat2 == "A", "Oui", "Non"))
-
-Covariate <- cbind(Covariate, var_cat, cat1, cat2, cat3)
+#var_cat <- as.factor(matrix(sample(letters[1:4], nrow(Covariate), replace = TRUE), ncol = 1))
+# cat1 = as.factor(matrix(sample(letters[1:3], nrow(Covariate), replace = TRUE), ncol = 1))
+# cat2 = as.factor(matrix(sample(LETTERS[1:2], nrow(Covariate), replace = TRUE), ncol = 1))
+# cat3 = as.factor(ifelse(cat2 == "A", "Oui", "Non"))
+# 
+# Covariate <- cbind(Covariate, var_cat, cat1, cat2, cat3)
 
 Covariate$rainfall <- rowSums(Covariate[, 27:38])
 Covariate$vegetation_index <- rowSums(Covariate[, 1:23])
@@ -57,7 +57,7 @@ data_to_PLN <- PLNmodels::prepare_data(Abundance, Covariate)
 prep_Covar_environement <- data_to_PLN[ , c("vegetation_index","rainfall",
                                             "altitude","awd","mcwd", "wetness",
                                             "CWD","center_x", "center_y",
-                                            "pluvio_an", "surface", "var_cat", "cat1", "cat2", "cat3")]  # , "var_cat", "cat1", "cat2", "cat3"
+                                            "pluvio_an", "surface")]  # , "var_cat", "cat1", "cat2", "cat3"
 
 # Calcul des VIF (variance inflation factors)
 Res_vif <- calculate_VIF(prep_Covar_environement, 5)
@@ -94,32 +94,61 @@ data <- data_to_PLN
 genus2_sicpln <- SICPLN(formule, offset_column_name = "surface", data = data_to_PLN, numb_eps = 100, maxIter = 300, control = control)
 genus2_sicpln$res_fisher$model_par$B
 
+
+# Extract abundance and covariate matrices
+Abundance_matrix <- data$Abundance
+Covariate_matrix <- data[, colnames(retain_Covar)]
+Covariate_matrix <- Covariate_matrix[, colnames(Covariate_matrix) != offset_variable]
+# Check the structure of the matrices and vectors
+str(Abundance_matrix)
+str(Covariate_matrix)
+
+genus_glmnet <- glmnet_lasso_poisson(Abundance = data$Abundance,
+                                     Covariate = Covariate_matrix,
+                                     offset_column_name = "surface",
+                                     data = data,
+                                     verbose = TRUE)
+
+genus_glmnet$hat_B
+genus_glmnet$prediction
+
+
 # Convertir la matrice en un format adapté à ggplot2
 data_df <- melt(data$Abundance)
 data_sic <- melt(genus2_sicpln$fitted)
-
+data_glmnet <- melt(genus_glmnet)
 
 # Renommer les colonnes
 colnames(data_df) <- c("Site", "Species", "Comptage")
 colnames(data_sic) <- c("Site", "Species", "Comptage")
-
+colnames(data_glmnet) <- c("Site", "Species", "Comptage")
 # Graphiques
 plot_coef_pln <- coef_genus_barre(genus2_pln_offset$model_par$B[-1,], 
-                                  axis_names = c("Genus", "Variables", "Coefficients value", "PLN"), -3, 3)
-plot_coef_sicpln <- coef_genus_barre(genus2_sicpln$res_fisher$model_par$B[-1,], axis_names = c("Genus species", "Variables", "Coefficients value", "SICPLN"), -3, 3)
+                                  axis_names = c("Genus", "Variables", "Coefficients value", "PLN"), data = data)
+plot_coef_sicpln <- coef_genus_barre(genus2_sicpln$res_fisher$model_par$B[-1,],
+                                     axis_names = c("Genus species", "Variables", "Coefficients value", "SICPLN"), data = data)
+plot_coef_glmnet <- coef_genus_barre(genus_glmnet$hat_B[-1,],
+                                     axis_names = c("Genus species", "Variables", "Coefficients value", "GLMNET"), data = data)
+
 
 # plot_coef_pln + geom_hline(yintercept = 3.5) + geom_hline(yintercept = 5.5)
 
 plt_abund <- boxplot_graph(data_df = data_df, x = "Species", y = "Comptage", title = "Boxplot of Abundances by Species")
 plt_abund_sic <- boxplot_graph(data_df = data_sic, x = "Species", y = "Comptage", title = "Boxplot of Abundances fitted by Species")
+plt_abund_glmnet <- boxplot_graph(data_df = data_glmnet, x = "Species", y = "Comptage", title = "Boxplot of Abundances fitted by Species")
 
 # Utilisation de la fonction avec les données d'abondance et les valeurs prédites
 resid_pln <- plot_residus_density(data$Abundance, genus2_pln_offset$fitted, output_dir) #, output_dir
 resid_sicpln <- plot_residus_density(data$Abundance, genus2_sicpln$fitted, output_dir) #, output_dir
+resid_glmnet <- plot_residus_density(data$Abundance, genus_glmnet$fitted, output_dir) #, output_dir
+
+
 
 # # density des y et densité des y prédit
 density_abundance_pln <- plot_density(data$Abundance, genus2_pln_offset$fitted, main = "Density Plot of Abundance from PLN", xlab = "Abundance")
 density_abundance_sicpln <- plot_density(data$Abundance, genus2_sicpln$fitted, main = "Density Plot of Abundance from SICPLN", xlab = "Abundance")
+density_abundance_glmnet <- plot_density(data$Abundance, genus_glmnet$fitted, main = "Density Plot of Abundance from GLMNET", xlab = "Abundance")
+
 
 # image de la matrice sigma (precision matrix)
 tmp <- solve(genus2_sicpln$res_fisher$model_par$Sigma)
@@ -131,20 +160,36 @@ precision_mat_sicpln <- plot_precision_matrix(cor_mat, axis_names = c("", "", ""
 label_order <- paste0("gen",1:15,sep = "")
 MSE_pln_graph <- plot_mse_barplot(data$Abundance, genus2_pln_offset$fitted, label_order = label_order)
 MSE_sicpln_graph <- plot_mse_barplot(data$Abundance, genus2_sicpln$fitted, label_order = label_order)
+MSE_glmnet_graph <- plot_mse_barplot(data$Abundance, genus_glmnet$fitted, label_order = label_order)
 
 # # Relation entre variable environnementale et Espèce
-plotcovar_Abund <- plot_abundance_vs_environment(genus2_sicpln$res_fisher$model_par$B, data, output_dir) #, output_dir
+plotcovar_Abund_sic <- plot_abundance_vs_environment(genus2_sicpln$res_fisher$model_par$B, data, output_dir) #, output_dir
+#plotcovar_Abund_glmnet <- plot_abundance_vs_environment(genus_glmnet$hat_B, data, output_dir) #, output_dir
+
+combined_density_plot <- plot_combined_density(data$Abundance, genus2_pln_offset$fitted,genus2_sicpln$fitted, genus_glmnet$fitted,
+                                               main = "Combined Density Plot of Abundance", xlab = "Abundance")
 
 # Sauvegarde des graphiques dans le dossier de sortie spécifié en argument
 ggsave(file.path(output_dir, "plot_pln.pdf"), plot_coef_pln, width = 10, height = 6, units = "in")
 ggsave(file.path(output_dir, "plot_sicpln.pdf"), plot_coef_sicpln, width = 10, height = 6, units = "in")
+ggsave(file.path(output_dir, "plot_glmnet.pdf"), plot_coef_glmnet, width = 10, height = 6, units = "in")
+
+
 ggsave(file.path(output_dir, "plt_abund.pdf"), plt_abund, width = 10, height = 6, units = "in")
 ggsave(file.path(output_dir, "plt_abund_sic.pdf"), plt_abund_sic, width = 10, height = 6, units = "in")
+ggsave(file.path(output_dir, "plt_abund_glmnet.pdf"), plt_abund_glmnet, width = 10, height = 6, units = "in")
+
 ggsave(file.path(output_dir, "density_abundance_pln.pdf"), density_abundance_pln, width = 10, height = 6, units = "in")
 ggsave(file.path(output_dir, "density_abundance_sicpln.pdf"), density_abundance_sicpln, width = 10, height = 6, units = "in")
+ggsave(file.path(output_dir, "density_abundance_glmnet.pdf"), density_abundance_glmnet, width = 10, height = 6, units = "in")
+
 ggsave(file.path(output_dir, "precision_mat_sicpln.pdf"), precision_mat_sicpln, width = 10, height = 6, units = "in")
 ggsave(file.path(output_dir, "MSE_pln_graph.pdf"), MSE_pln_graph, width = 10, height = 6, units = "in")
 ggsave(file.path(output_dir, "MSE_sicpln_graph.pdf"), MSE_sicpln_graph, width = 10, height = 6, units = "in")
+ggsave(file.path(output_dir, "MSE_glmnet_graph.pdf"), MSE_glmnet_graph, width = 10, height = 6, units = "in")
+
+ggsave(file.path(output_dir, "combined_density_plot.pdf"), combined_density_plot, width = 10, height = 6, units = "in")
+
 # ggsave(file.path(output_dir, "resid_pln.pdf"), resid_pln, width = 10, height = 6, units = "in")
 # ggsave(file.path(output_dir, "resid_sicpln.pdf"), resid_sicpln, width = 10, height = 6, units = "in")
 
