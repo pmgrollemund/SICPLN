bd95f9e05b61e54e65b2c34497f9db0398ed184d
REMESHA <118030182+REMESHA@users.noreply.github.com>
2024-09-17 11:29:33 +0200

    Create packages.R

---
+++ code/packages.R
@@ -0,0 +1,34 @@
+################################################################################ ----
+#######
+# Required packages 
+#######
+################################################################################ ----
+
+suppressPackageStartupMessages(library("torch"))
+suppressPackageStartupMessages(library("glmnet"))
+suppressPackageStartupMessages(library("PLNmodels"))
+suppressPackageStartupMessages(library("Matrix"))
+suppressPackageStartupMessages(library("MASS"))
+suppressPackageStartupMessages(library("ggplot2"))
+suppressPackageStartupMessages(library("reshape2"))
+suppressPackageStartupMessages(library("ggpattern"))
+suppressPackageStartupMessages(library("svMisc"))
+suppressPackageStartupMessages(library("rlang"))
+suppressPackageStartupMessages(library("tidyr"))
+suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("RColorBrewer"))
+suppressPackageStartupMessages(library("gplots"))
+suppressPackageStartupMessages(library("viridis"))
+suppressPackageStartupMessages(library("factoextra"))
+suppressPackageStartupMessages(library("tidyverse"))
+################################################################################ ----
+#######
+# Required objects 
+#######
+################################################################################ ----
+
+
+
+
+################################################################################ ----
+#######
